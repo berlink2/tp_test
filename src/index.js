424a1bfe868c6f33519652de1206fc099e416759
berlink2 <berlink.k2@gmail.com>
2020-08-30 21:12:16 +0700

    node-sass fix

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import App from './app';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./app";
 
-
+//node node_modules/node-sass/scripts/install.js
 
 ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
-  
-  
-  
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
